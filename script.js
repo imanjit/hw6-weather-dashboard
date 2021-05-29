bbb2189aea7f2b5604cd0530b3c27ae5ecffa3b2
Iman Bains <imanjitbains@yahoo.com>
2021-05-29 15:20:46 -0700

    Added function to fetch forecast

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-let apiKey = "c32994fa4154612f41f38bdd2aa60666";
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+let savedCities = []
 
+let apiKey = "c32994fa4154612f41f38bdd2aa60666";
+
+savedCities.forEach(function (city, index, arr) {
+    if (index === arr.length - 1) {
+        fetchForecast(city)
+    }
+})
+
+function fetchForecast(city) {
+    let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    fetch(apiUrl)
+        .then(function (response) {
+            let temperature = response.main.temp;
+            let windSpeed = response.wind.speed;
+            let humidity = response.main.humidity
+
+            let cityDiv = $("<div class='city'>");
+            let title = $("<h3>").text(city);
+            let tem = $("<p>").text("Temperature: " + temperature + "F");
+            let win = $("<p>").text("Wind Speed: " + windSpeed + "mph");
+            let hum = $("<p>").text("Humidity: " + humidity + "%");
+
+            cityDiv.append(title, tem, win, hum);
+
+            $("#todayWeather").empty();
+            $("todayWeather").prepend(cityDiv);
+        })
+}
+
+$("#searchBtn").on("click", function(event){
+    event.preventDefault();
+
+    let weather = $("#cityInput").val();
+
+    localStorage.setItem("weather", JSON.stringify(savedCities))
+    savedCities.push(weather);
+
+})
